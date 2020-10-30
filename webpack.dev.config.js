001ceb72b54da28e498cbbd5b47d21516f0758ad
鲁攀 <wb-lp789303@alibaba-inc.com>
2020-10-30 18:32:12 +0800

    create

---
+++ webpack.dev.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname,
+    filename: './dist/bundle.js'  // dist 会自动创建
+  },
+
+  module: {
+    rules: [{
+      test: /\.js?$/,
+      exclude: /(node_modules)/,
+      loader: "babel-loader"
+    }]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, './dist'), // 启动根目录
+    open: true, // 自动打开浏览器
+    port: 9000
+  }
+}
